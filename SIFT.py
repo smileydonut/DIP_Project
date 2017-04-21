99b7dcc104532cbbc90eee39ac8742bc4a6052fc
achoo486 <saliraza486@gmail.com>
2017-04-21 18:49:18 +0500

    Update SIFT.py

--- SIFT.py
+++ SIFT.py
@@ -1,34 +1,51 @@
 import numpy as np
 import cv2
 
-blur_levels = 5;
-octave_levels = 4;
-cap = cv2.VideoCapture(0);
+cap = cv2.VideoCapture('test_video.mp4');
+#octaves_set contains all octaves_set_single_frame
+octaves_set = [];
+frame_count = 0;
 while(cap.isOpened()):
     ret, frame = cap.read();
     if ret == False:
         break;
 
+    frame_count = frame_count + 1;
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY);
+
+    blur_levels = 5;
+    octave_levels = 4;
+
+    #making room for new octaves_set_single_frame
+    octaves_set.append([]);
+
+    octaves_set_single_frame = [];
 
     k = 2;
     sigma = 1;
 
-    for j in range(octave_levels):
+    for o_level in range(octave_levels):
+        #adding new octave level
+        octaves_set_single_frame.append([])
         #sigma starting point changes by a factor of sqrt(2) after every octave
         sigma = np.sqrt(2) * 2;
-        for i in range(blur_levels):
-            blurred_frame = cv2.GaussianBlur(frame, (5, 5), sigma);
-            if i == 0 and j == 0:
-                cv2.imshow('Level 1', blurred_frame);
-            if i == 4 and j == 1:
-                cv2.imshow('Level 4', blurred_frame);
 
+        for b_level in range(blur_levels):
+            blurred_frame = cv2.GaussianBlur(frame, (5, 5), sigma);
+            octaves_set_single_frame[o_level].append(blurred_frame);
+            # if b_level == 0 and o_level == 0:
+            #     cv2.imshow('Level 1', blurred_frame);
+            # if b_level == 4 and o_level == 1:
+            #     cv2.imshow('Level 4', blurred_frame);
             sigma = k * sigma;
 
         frame_dim = np.array(np.shape(frame));
         frame_dim = (frame_dim / 2).astype(np.uint32);
+        #flipped frame_dim because in cv2.resize no: of columns come first
         frame = cv2.resize(frame, tuple(np.flip(frame_dim, 0)));
+
+    #add current octaves_set_single_frame at the last location (which is initialized as empty sublist [])
+    octaves_set[-1].append(octaves_set_single_frame);
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break;
